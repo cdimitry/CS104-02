4ac5cc1a3fceb66e64db442789d9dab2ed583a1b
cdimitry <71794403+cdimitry@users.noreply.github.com>
2020-09-30 17:50:18 -0400

    Average program before loop and correct print statement

--- Average.py
+++ Average.py
@@ -1,2 +1,18 @@
-numberofscores=input("please enter the number of scores you want to average")
-print(numberofscores)
+#initialize all variables to 0
+numberOfScores = 0
+score = 0
+total = 0
+scoreCount = 0
+average = 0.0
+
+#Accept the number of scores to average
+numberOfScores = input("please enter the number of scores you want to average")
+
+score = int(input("please enter a score:")
+total = total + score
+#Add a loop to make this code until scoreCount = numberOfScores
+scoreCount = scoreCount + 1
+
+
+average = total / numberOfScores
+print(average)
